d7e81f5b192a793140a18a612f5f63cb6214e2d4
Ilya <110740644+volkaris@users.noreply.github.com>
2023-04-04 20:52:22 +0300

    Update Kruskul.cpp

--- Kruskul.cpp
+++ Kruskul.cpp
@@ -1,58 +1,55 @@
-﻿#include <algorithm>
+#include <algorithm>
 #include <iostream>
 #include <vector>
-
+#include <tuple>
+#include <ranges>
 
 using namespace std;
-
 
 int main() {
 	int V, E;
 	cin >> V >> E;
-	int how_much_edges_to_add = V - 1;
 
-	vector<vector<pair<int, int>>> graph(V + 1);
-
-	std::vector<std::tuple<int, int, int>> ordered_edges;
+	std::vector<std::tuple<int, int, int>> edges(E);
 	std::vector<std::tuple<int, int, int>> MST;
-	std::vector<int> colours(V + 1);
+	MST.reserve(V - 1);
+
+	std::vector<int> colors(V + 1);
+
 	for (int i = 0; i < E; i++) {
 		int u, v, w;
 		cin >> u >> v >> w;
-		graph[u].emplace_back(v, w);
-		graph[v].emplace_back(u, w);
-		ordered_edges.emplace_back(u, v, w);
+		edges[i] = std::make_tuple(u, v, w);
 	}
+
+	std::ranges::sort(edges, [](const auto& a, const auto& b) {
+		return std::get<2>(a) < std::get<2>(b);});
 
 	for (int i = 1; i <= V; i++) {
-		colours[i] = i;
+		colors[i] = i;
 	}
-	bool found = false;
-
-	ranges::sort(ordered_edges, [](const auto& a, const auto& b) {
-		return std::get<2>(a) < std::get<2>(b);
-	});
-
-
 	int idx = 0;
-	for (;;) {
-		for (int i = 0; i < ordered_edges.size(); ++i) {
-			if (colours[std::get<1>(ordered_edges[i])] != colours[std::get<0>(ordered_edges[i])]) {
-				MST.emplace_back(ordered_edges[i]);
-				colours[std::get<1>(ordered_edges[i])] = colours[std::get<0>(ordered_edges[i])];
 
+	for (const auto& edge : edges) {
+		int start = std::get<0>(edge);
+		int end = std::get<1>(edge);
 
-				++idx;
-				if (idx == how_much_edges_to_add) {
-					found = true;
-					break;
-				}
+		if (colors[start] != colors[end]) {
+
+			MST.emplace_back(edge);
+
+			int old_color = colors[end];
+
+			ranges::transform(colors, colors.begin(),[&](int c) {
+				return c == old_color ? colors[start] : c;
+			});
+			++idx;
+			if (idx == V - 1) {
+				break;
 			}
-		}
 
-		if (found) {
-			break;
 		}
 	}
-	int x = 10;
+
+
 }
