360056f9487b287c32dad72f0768a56b346fde08
Ilya <110740644+volkaris@users.noreply.github.com>
2023-04-05 23:22:07 +0300

    Update kosaraju.cpp

--- kosaraju.cpp
+++ kosaraju.cpp
@@ -1,75 +1,88 @@
-﻿#include <iostream>
+#include <iostream>
+#include <stack>
 #include <vector>
-
+#include <unordered_set>
 using namespace std;
 
 
-
-bool dfs(size_t current_vertex, size_t current_depth, size_t num_visited, vector<bool>& visited, const vector<vector < size_t >>& graph, size_t n) {
-    visited[current_vertex] = true;
-    ++num_visited;
-    if (num_visited == n) {
-        return true;
-    }
-    for (size_t i = 0; i < graph[current_vertex].size(); ++i) {
-        const size_t to = graph[current_vertex][i];
-        if (!visited[to]) {
-            if (dfs(to, current_depth + 1, num_visited, visited, graph, n)) {
-                return true;
-            }
+vector<unordered_set<size_t>> strongly_connected_comp;
+size_t scc = 1;
+void dfs(vector<vector<size_t>>& adj, size_t u, vector<bool>& visited, stack<size_t>& stk) {
+    visited[u] = true;
+    for (size_t v : adj[u]) {
+        if (!visited[v]) {
+            dfs(adj, v, visited, stk);
         }
     }
-    visited[current_vertex] = false;
-    return false;
+    stk.push(u);
 }
 
-void dfs_count_components(size_t current_vertex, vector<bool>& visited, const vector<vector < size_t >>& graph) {
-    visited[current_vertex] = true;
-    for (size_t i = 0; i < graph[current_vertex].size(); ++i) {
-        const size_t to = graph[current_vertex][i];
-        if (!visited[to]) {
-            dfs_count_components(to, visited, graph);
+void reverse_dfs(vector<vector<size_t>>& adj, size_t u, vector<bool>& visited) {
+    visited[u] = true;
+    strongly_connected_comp[scc].emplace(u);
+    for (size_t v : adj[u]) {
+        if (!visited[v]) {
+            
+            reverse_dfs(adj, v, visited);
         }
     }
+}
+
+void kosaraju(size_t V, vector<vector<size_t>>& adj) {
+    vector visited(V + 1, false);
+    stack<size_t> stk;
+    for ( size_t i = 1; i <= V; i++) {
+        if (!visited[i]) {
+            dfs(adj, i, visited, stk);
+        }
+    }
+
+    vector<vector<size_t>> trans_adj(V + 1);
+    for (size_t i = 1; i <= V; i++) {
+        visited[i] = false;
+        for (size_t j : adj[i]) {
+            trans_adj[j].push_back(i);
+        }
+    }
+   
+    while (!stk.empty()) {
+        size_t u = stk.top();
+        stk.pop();
+        if (!visited[u]) {
+            reverse_dfs(trans_adj, u, visited);
+            scc++;
+        }
+    }
+
 }
 
 int main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    size_t n, m;
-    cin >> n >> m;
-   
-    vector<vector < size_t >> graph;
-    vector<bool> visited;
-    graph.resize(n + 1);
-    visited.assign(n + 1, false);
-    for (size_t i = 0; i < m; ++i) {
-        int u, v;
+    size_t n, m,q;
+    cin >> n >> m>>q;
+    vector<vector<size_t>> adj(n + 1);
+    for (size_t i = 0; i < m; i++) {
+        size_t u, v;
         cin >> u >> v;
-        graph[u].push_back(v);
-        graph[v].push_back(u);
+        adj[u].push_back(v);
     }
+    strongly_connected_comp.resize(n + 1);
+    kosaraju(n, adj);
 
-
-    for (size_t i = 1; i <= n; ++i) {
-        if (!visited[i]) {
-            dfs_count_components(i, visited, graph);
+    for (size_t requests=0;requests<q;++requests) {
+        size_t start, end;
+        cin >> start >> end;
+        bool found = false;
+        for(const auto& i:strongly_connected_comp) {
+	        if (i.contains(start) and i.contains(end)) {
+                cout << "YES"<<"\n";
+                found = true;
+                break;
+	        }
            
         }
-    }
-
-   
-
-
-    visited.assign(n + 1, false);
-    for (size_t i = 1; i <= n; ++i) {
-        if (!visited[i]) {
-            if (dfs(i, 1, 0, visited, graph, n)) {
-                cout << "YES";
-                return 0;
-            }
+        if (!found) {
+            cout << "NO\n";
         }
     }
 
-    cout << "NO";
 }
